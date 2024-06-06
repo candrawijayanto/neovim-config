97012caa0f8c2b005c86b94f1b766e3a4152d1a0
candrawijayanto <candra@email.com>
2024-06-06 17:45:00 +0700

    refactor neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,10 +4,8 @@ return {
         event_handlers = {
             {
                 event = "neo_tree_buffer_enter",
-                handler = function(arg)
-                    vim.cmd([[
-              setlocal relativenumber
-              ]])
+                handler = function()
+                    vim.cmd("setlocal relativenumber")
                 end,
             },
         },
